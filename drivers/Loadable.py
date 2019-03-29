18373d3c25f5b9acc113cc282f2a1853422e029c
Coding-Badly <arduino.tiny@gmail.com>
2019-03-28 23:57:58 -0500

    Add automated testing for Loadable.  Minor cleanup.

--- drivers/Loadable.py
+++ drivers/Loadable.py
@@ -1,8 +1,33 @@
+"""=============================================================================
+
+  Loadable for RFID-KeyMaster.  Loadable is a base class for all run-time
+  loadable drivers.  Each Loadable is also a Thread allowing it to block
+  without interfering with other parts of the application.  Loadable provides
+  three services: 1. Connection to configuration data and a parent; 2. Find
+  siblings so drivers can interact directly with each other; 3. Turn all
+  unexpected exceptions into a clean exit.
+  
+  ----------------------------------------------------------------------------
+
+  Copyright 2019 Mike Cole (aka @Draco, MikeColeGuru)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+============================================================================="""
 import threading
 from exceptions.RequiredDriverException import RequiredDriverException
 import logging
 import os
-import time
 from pydispatch import Dispatcher
 
 class Loadable(threading.Thread, Dispatcher):
