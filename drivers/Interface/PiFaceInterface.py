65a7f80c2c6ef6306dc7e2dade4672478a0ba279
Coding-Badly <arduino.tiny@gmail.com>
2019-04-29 18:27:08 -0500

    Start reworking PiFaceInterface.py.

--- drivers/Interface/PiFaceInterface.py
+++ drivers/Interface/PiFaceInterface.py
@@ -1,21 +1,52 @@
-from drivers.Interface.Interface import Interface
+"""=============================================================================
+
+  PiFaceInterface for RFID-KeyMaster.  PiFaceInterface is a driver for the
+  PiFace Digital 2 HAT.  The PiFace is used to control power to the target,
+  receive input from a pushbutton, and drive an LED.
+
+  ----------------------------------------------------------------------------
+
+  Copyright 2019 Mike Cole (aka @Draco, MikeColeGuru)
+  Copyright 2019 Brian Cook (aka @Brian, Coding-Badly)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+============================================================================="""
+from drivers.Interface import Interface
 import pifacedigitalio
 from exceptions.InvalidPositionException import InvalidPositionException
-import atexit
-
+# rmv import atexit
 
 class PiFaceInterface(Interface):
     def setup(self):
+        super().setup()
+        init_board = self.config.get('init_board', False)
+        self._target = int(self.config.get('target', 0))
         # Turn off Interrupts
         pifacedigitalio.core.deinit()
-        self.pifacedigital = pifacedigitalio.PiFaceDigital()
-        atexit.register(self.reset_piface)
-        
-        return False
-        
-    def reset_piface(self):
-        self.pifacedigital.init_board()
-        
+        self.pifacedigital = pifacedigitalio.PiFaceDigital(init_board=init_board)
+        self.subscribe(None, Interface.CONTROL_TARGET, self.receive_control_target)
+        # rmv atexit.register(self.reset_piface)
+        #return False
+    def startup(self):
+        super().startup()
+        self.open_for_business()
+    def receive_control_target(self, data):
+        pass
+
+    # rmv def reset_piface(self):
+    # rmv     self.pifacedigital.init_board()
+
     def relay(self, position, value):
         position = int(position)
 
