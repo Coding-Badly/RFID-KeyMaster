5254786404e58444047a1260e43ec1db31bfbb75
Coding-Badly <arduino.tiny@gmail.com>
2019-04-29 19:18:26 -0500

    Add code to PiFaceInterface.py to actually manipulate the target relay.

--- drivers/Interface/PiFaceInterface.py
+++ drivers/Interface/PiFaceInterface.py
@@ -34,18 +34,20 @@ class PiFaceInterface(Interface):
         self._target = int(self.config.get('target', 0))
         # Turn off Interrupts
         pifacedigitalio.core.deinit()
-        self.pifacedigital = pifacedigitalio.PiFaceDigital(init_board=init_board)
+        self._pifacedigital = pifacedigitalio.PiFaceDigital(init_board=init_board)
         self.subscribe(None, Interface.CONTROL_TARGET, self.receive_control_target)
         # rmv atexit.register(self.reset_piface)
         #return False
     def startup(self):
         super().startup()
+        # fix? Publish the initial state of the target?
         self.open_for_business()
     def receive_control_target(self, data):
-        pass
+        self._pifacedigital.relays[self._target].value = int(data)
+        # fix? Publish that the target state has changed?
 
     # rmv def reset_piface(self):
-    # rmv     self.pifacedigital.init_board()
+    # rmv     self._pifacedigital.init_board()
 
     def relay(self, position, value):
         position = int(position)
@@ -61,7 +63,7 @@ class PiFaceInterface(Interface):
         position = int(position)
 
         if position >= 1 and position <= 8:
-            return self.pifacedigital.input_pins[position-1].value
+            return self._pifacedigital.input_pins[position-1].value
 
         raise InvalidPositionException(
             "PiFace has no input position " + str(position))
@@ -76,9 +78,9 @@ class PiFaceInterface(Interface):
 
         if position >= 1 and position <= 8:
             if value:
-                self.pifacedigital.output_pins[position-1].turn_on()
+                self._pifacedigital.output_pins[position-1].turn_on()
             else:
-                self.pifacedigital.output_pins[position-1].turn_off()
+                self._pifacedigital.output_pins[position-1].turn_off()
             return
 
         raise InvalidPositionException(
