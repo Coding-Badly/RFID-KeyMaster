00fefb6afa6b1ca3b320ba3b5456fbd52c7d7139
Coding-Badly <arduino.tiny@gmail.com>
2019-07-29 16:10:12 -0500

    Change KeyMaster Setup (kms) back to explicitly installing python-dispatch.  Installing through PIP makes the package an anomaly.  Continue work on the KeyMaster class.

--- rfidkm/__init__.py
+++ rfidkm/__init__.py
@@ -19,9 +19,73 @@
   limitations under the License.
 
 ============================================================================="""
+import json
+
+from rfidkm.utils.Configuration import Configuration
+from rfidkm.utils.LoadableDriverBuilder import LoadableDriverBuilder
+
+# cls & pytest rfidkm/tests/test_KeyMaster.py
 
 class KeyMaster():
+    __slots__ = ['_root', '_is_running']
+    def __init__(self):
+        self._root = None
+        self._is_running = False
+    def build_from_loaded_json(self, converted_config):
+        assert self._root is None
+        wrapped_config = Configuration(converted_config)
+        config_builder = LoadableDriverBuilder(wrapped_config)
+        self._root = config_builder.build()
+    def build_from_configuration_file(self, config_path):
+        with config_path.open('r') as inf:
+            converted_config = json.load(inf)
+        self.build_from_loaded_json(converted_config)
+        return converted_config  # rmv
+    def setup_and_start(self):
+        if not self._is_running:
+            self._root.setup()
+            self._root.start()
+            self._is_running = True
+    def wait_for_stop(self):
+        if self._is_running:
+            self._root.join()
+            self._root.teardown()
+            self._is_running = False
     #def make_power_controller(self, group_name):
     #    pass
-    def build_from_configuration_file(self, path):
-        pass
+
+"""
+import json
+
+json.loads("")
+
+    tm1 = LoadableDriverBuilder(Configuration(raw_config_001))
+    root = tm1.build()
+    root.setup()
+    root.start()
+    root.join()
+    root.teardown()
+
+        tm1 = LoadableDriverBuilder(Configuration(raw_config))
+        raw_config['primary'][2]['expected_current_was_flowing'] = expected_current_was_flowing
+        raw_config['primary'][2]['expected_relay_was_closed'] = expected_relay_was_closed
+        root = tm1.build()
+        root.setup()
+        root.start()
+        root.join()
+        root.teardown()
+
+    config = Configuration(raw_config_001)
+    root = DriverGroup('root')
+    dor = root.add(DeathOfRats(config=config))
+    for group_name in config['groups']:
+        group_root = root.add(DriverGroup(group_name))
+        group_driver_list = config[group_name]
+        for driver_config in group_driver_list:
+            driver_name = driver_config['driver']
+            logger.info(driver_name)
+
+C:\ProjectsSplit\Make\RFID-KeyMaster\rfidkm\tests\test_LoadableDriverLoader.py
+
+C:\ProjectsSplit\Make\RFID-KeyMaster\rfidkm\tests\test_MemberDataFreshener.py
+"""
